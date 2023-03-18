f73f9e50b6f625198be30efefd777a85834f1c35
Silas Rodrigues <Silas Rodrigues@DEL7KDSZ02>
2023-03-18 19:47:13 -0300

    Add project files.

---
+++ Contact/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Esta linha de código carrega dados na tabela 'contactDBDataSet.People'. Você pode mover, ou removê-lo, se necessário
+            this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+        }
+
+       
+    }
+}
